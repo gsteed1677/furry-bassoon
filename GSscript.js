9683b6b3e5b155c6ca11427ddfd8ebc3d2697c5a
Ashley Bartholomew <ashley.52389@gmail.com>
2020-11-05 21:45:14 -0700

    pushing changes

--- GSscript.js
+++ GSscript.js
@@ -1,119 +1,82 @@
 var saveToLocalStorage = function (name) {
-
-  var recent = localStorage.getItem('recentPokemon');
-
-  var json = [];
-  
-  if (recent) {
-    json = JSON.parse(recent);
+    var recent = localStorage.getItem('recentPokemon');
+    var json = [];
+    if (recent) {
+      json = JSON.parse(recent);
+    }
+    json.push(name);
+    localStorage.setItem('recentPokemon', JSON.stringify(json))
   }
-
-  json.push(name);
-
-  localStorage.setItem('recentPokemon', JSON.stringify(json))
-
-}
-
-var loadFromLocalStorage = function () {
-  var recent = localStorage.getItem('recentPokemon');
-
-  if (!recent) return [];
-
-  var json = JSON.parse(recent);
-  return json.reverse();
-}
-
-
-var renderRecentPokemon = function () {
-
-  var recentPokemon = loadFromLocalStorage();
-  $('#recent-pokemon').text('Recent Searchs: ');
-
-  for (var i = 0; i < recentPokemon.length; i++) {
-    var btn = $("<button>");
-    btn.text(recentPokemon[i])
-    btn.click({ name: recentPokemon[i] }, function (e) {
-      getPokeApi(e.data.name)
-    })
-    $('#recent-pokemon').append(btn);
+  var loadFromLocalStorage = function () {
+    var recent = localStorage.getItem('recentPokemon');
+    if (!recent) return [];
+    var json = JSON.parse(recent);
+    return json.reverse();
   }
-
-}
-
-$(document).ready(function () {
-
-  $("#searchClick").on("click", function () {
-    var pokemonName = $("#textInput").val();
-    // console.log(pokemonName)
-    $("#textInput").val("")
-
-    getPokeApi(pokemonName, true);
-  });
-  renderRecentPokemon();
-})
-
-
-var getPokeApi = function (pokemonName, saveToLocal) {
-  var requestURL = "https://pokeapi.co/api/v2/pokemon/" + (pokemonName || '').trim().toLowerCase();
-  //AJAX call
-  $.ajax({
-    url: requestURL,
-    method: 'GET',
-  }).then(function (data) {
-
-    //second fetch here:
-    // fetch(dataCard) {
-    
-   // }
-
-
-
-
-    if (saveToLocal) {
-      saveToLocalStorage(data.name);
-      renderRecentPokemon();
+  var renderRecentPokemon = function () {
+    var recentPokemon = loadFromLocalStorage();
+    $('#recent-pokemon').text('Recent Searchs: ');
+    for (var i = 0; i < recentPokemon.length; i++) {
+      var btn = $("<button>");
+      btn.text(recentPokemon[i])
+      btn.click({ name: recentPokemon[i] }, function (e) {
+        getPokeApi(e.data.name)
+      })
+      $('#recent-pokemon').append(btn);
     }
-
-    $("#display-pokemon").empty();
-
-    var pokemon = $("#display-pokemon");
-    var pokeName = $("<h3>");
-    var pokeMove = $("<p>");
-    var pokeType = $("<p>");
-    var pokeIndex = $("<p>");
-
-    var str = "Moves: "
-    console.log(data)
-    function randArr() {
-    var movesArr = [];
-    for (var i = 0; i < 5; i++) {
-    
-    var random = data.moves[Math.floor(Math.random() * 102)];
-    movesArr.push(random);
-    console.log(random)
-    str += data.moves[i].move.name + " "
-    }
-    console.log(movesArr)
-    }
-    randArr();
-
-    for (var i = 0; i < data.moves.length; i++) {
-    }
-
-
-
-    pokeMove.text(str)
-    pokeName.text("Name: " + data.name.toUpperCase());
-    pokeType.text("Type: " + data.types[0].type.name.toUpperCase());
-    pokeIndex.text("Pokedex #: " + data.id);
-    pokemon.append(pokeName);
-    pokemon.append(pokeMove);
-    pokemon.append(pokeType);
-    pokemon.append(pokeIndex);
-
+  }
+  $(document).ready(function () {
+    $("#searchClick").on("click", function () {
+      var pokemonName = $("#textInput").val();
+      // console.log(pokemonName)
+      $("#textInput").val("")
+      if (!pokemonName) {
+          return;
+      }
+      getPokeApi(pokemonName, true);
+    });
+    renderRecentPokemon();
   })
-}
-
-
-
-//Event listener
+  var getPokeApi = function (pokemonName, saveToLocal) {
+    var requestURL = "https://pokeapi.co/api/v2/pokemon/" + (pokemonName || '').trim().toLowerCase();
+    //AJAX call
+    $.ajax({
+      url: requestURL,
+      method: 'GET',
+    }).then(function (data) {
+      if (saveToLocal) {
+        saveToLocalStorage(data.name);
+        renderRecentPokemon();
+      }
+      $("#display-pokemon").empty();
+      var pokemon = $("#display-pokemon");
+      var pokeName = $("<h3>");
+      var pokeMove = $("<p>");
+      var pokeType = $("<p>");
+      var pokeIndex = $("<p>");
+      var str = "Moves: "
+      console.log(data)
+      function randArr() {
+      var movesArr = [];
+      for (var i = 0; i < 5; i++) {
+      var random = data.moves[Math.floor(Math.random() * 102)];
+      movesArr.push(random);
+      console.log(random)
+      console.log("random", random.move.name)
+      str += random.move.name + " -- "
+      }
+      console.log(movesArr)
+      }
+      randArr();
+      for (var i = 0; i < data.moves.length; i++) {
+      }
+      pokeMove.text(str)
+      pokeName.text("Name: " + data.name.toUpperCase());
+      pokeType.text("Type: " + data.types[0].type.name.toUpperCase());
+      pokeIndex.text("Pokedex #: " + data.id);
+      pokemon.append(pokeName);
+      pokemon.append(pokeMove);
+      pokemon.append(pokeType);
+      pokemon.append(pokeIndex);
+    })
+  }
