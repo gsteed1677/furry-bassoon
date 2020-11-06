04c7e3694e82b5f2e89585b32992c07339e28e72
Garrett Steed <steed.garrett@gmail.com>
2020-11-06 15:15:45 -0700

    got local storage to bring back on stats

--- GSscript.js
+++ GSscript.js
@@ -1,118 +1,105 @@
-//function to save searched name to local storage
-var saveToLocalStorage = function (name) {
 
-  var recent = localStorage.getItem('recentPokemon');
-
-  var json = [];
-
-  if (recent) {
-    json = JSON.parse(recent);
-  }
-
-  json.push(name);
-
-  localStorage.setItem('recentPokemon', JSON.stringify(json))
-
-}
-//load data searches from local storage
-var loadFromLocalStorage = function () {
-  var recent = localStorage.getItem('recentPokemon');
-
-  if (!recent) return [];
-
-  var json = JSON.parse(recent);
-  return json.reverse();
-}
-
-//search function for user input of pokemon
-var renderRecentPokemon = function () {
-
-  var recentPokemon = loadFromLocalStorage();
-  $('#recent-pokemon').text('Recent Searchs: ');
-
-  for (var i = 0; i < recentPokemon.length; i++) {
-    //jquery of button creation for past pokemon search
-    var btn = $("<button>");
-    btn.text(recentPokemon[i])
-    btn.click({ name: recentPokemon[i] }, function (e) {
-      getPokeApi(e.data.name)
-    })
-    $('#recent-pokemon').append(btn);
-  }
-
-
-
-}
-//document ready to load all code
 $(document).ready(function () {
-//event listener
   $("#searchClick").on("click", function () {
     var pokemonName = $("#textInput").val();
-    // console.log(pokemonName)
-    $("#textInput").val("")
-
+    $("#textInput").val("");
     if (!pokemonName) {
       return;
     }
-
     getPokeApi(pokemonName, true);
+    apiPokemon(pokemonName);
   });
   renderRecentPokemon();
-})
-
-//fetch api request
-var getPokeApi = function (pokemonName, saveToLocal) {
-  var requestURL = "https://pokeapi.co/api/v2/pokemon/" + (pokemonName || '').trim().toLowerCase();
-  //AJAX call
-  $.ajax({
-    url: requestURL,
-    method: 'GET',
-  }).then(function (data) {
-
-
-    if (saveToLocal) {
-      saveToLocalStorage(data.name);
-      renderRecentPokemon();
-    }
-    //data display and creation of html tags
-    $("#display-pokemon").empty();
-
-    var pokemon = $("#display-pokemon");
-    var pokeName = $("<h3>");
-    var pokeMove = $("<p>");
-    var pokeType = $("<p>");
-    var pokeIndex = $("<p>");
-
-    var str = "Moves: "
-    console.log(data)
-    //function to randomize arrayed moves so each input is brought back differently
-    function randArr() {
-      var movesArr = [];
-      for (var i = 0; i < 5; i++) {
-
-        var random = data.moves[Math.floor(Math.random() * 102)];
-        movesArr.push(random);
-        console.log("random", random.move.name)
-        str += random.move.name + " -- "
+  
+  function getPokeApi(pokemonName, saveToLocal) {
+    var requestURL =
+      "https://pokeapi.co/api/v2/pokemon/" +
+      (pokemonName || "").trim().toLowerCase();
+    //AJAX call
+    $.ajax({
+      url: requestURL,
+      method: "GET",
+    }).then(function (data) {
+      if (saveToLocal) {
+        saveToLocalStorage(data.name);
+        renderRecentPokemon();
       }
-      console.log(movesArr)
-    }
-    randArr();
+      $("#display-pokemon").empty();
+      var pokemon = $("#display-pokemon");
+      var pokeName = $("<h3>");
+      var pokeMove = $("<p>");
+      var pokeType = $("<p>");
+      var pokeIndex = $("<p>");
+      var str = "Moves: ";
+      function randArr() {
+        var movesArr = [];
+        for (var i = 0; i < 5; i++) {
+          var random = data.moves[Math.floor(Math.random() * 102)];
+          movesArr.push(random);
+          str += random.move.name + " -- ";
+        }
+      }
+      randArr();
+      for (var i = 0; i < data.moves.length; i++) {}
+      pokeMove.text(str);
+      pokeName.text("Name: " + data.name.toUpperCase());
+      pokeType.text("Type: " + data.types[0].type.name.toUpperCase());
+      pokeIndex.text("Pokedex #: " + data.id);
+      pokemon.append(pokeName);
+      pokemon.append(pokeMove);
+      pokemon.append(pokeType);
+      pokemon.append(pokeIndex);
+    });
+  };
 
-    for (var i = 0; i < data.moves.length; i++) {
-    }
+  let apiPokemon = (pokemonName) => {
+    let requestURL = "https://api.pokemontcg.io/v1/cards?name=" + pokemonName;
+    $.ajax({
+      url: requestURL,
+      method: "GET",
+    }).then(function (data) {
+      // if (saveToLocal) {
+      //   saveToLocalStorage(data.cards[0].imageUrl);
+      //   renderRecentPokemon();
+      // }
+      console.log(data.cards[0]);
+      let pokemon = $("#pokemon-display");
+      let pokemonName = $("<img>");
+      pokemonName.attr("src", "" + data.cards[0].imageUrl);
+      pokemon.append(pokemonName);
 
+      
+    });
+    
+  };
+  function saveToLocalStorage(name) {
+  var recent = localStorage.getItem("recentPokemon");
+  var json = [];
+  if (recent) {
+    json = JSON.parse(recent);
+  }
+  json.push(name);
+  localStorage.setItem("recentPokemon", JSON.stringify(json));
+};
 
-    //text and append elements
-    pokeMove.text(str)
-    pokeName.text("Name: " + data.name.toUpperCase());
-    pokeType.text("Type: " + data.types[0].type.name.toUpperCase());
-    pokeIndex.text("Pokedex #: " + data.id);
-    pokemon.append(pokeName);
-    pokemon.append(pokeMove);
-    pokemon.append(pokeType);
-    pokemon.append(pokeIndex);
+function loadFromLocalStorage() {
+  var recent = localStorage.getItem("recentPokemon");
+  if (!recent) return [];
+  var json = JSON.parse(recent);
+  return json.reverse();
+};
 
-  })
-}
-
+function renderRecentPokemon() {
+  var recentPokemon = loadFromLocalStorage();
+  $("#recent-pokemon").text("Recent Searchs: ");
+  for (var i = 0; i < recentPokemon.length; i++) {
+    var btn = $("<button>");
+    btn.text(recentPokemon[i]);
+    btn.click({ name: recentPokemon[i] }, function (e) {
+      getPokeApi(e.data.name);
+      // apiPokemon(e.data.cards[0].imageUrl)
+    });
+    $("#recent-pokemon").append(btn);
+  }
+};
+});
--- TCGPokescript.js
+++ TCGPokescript.js
@@ -1,42 +1,47 @@
-$(document).ready(function(){ 
+// $(document).ready(function(){ 
 
-$("#searchClick").on("click", function(){
-        let pokemonCard = $("#textInput").val();
-        console.log(pokemonCard)
-        $("#textInput").val("")
-        apiPokemon(pokemonCard, true);
-    });
+//     $("#searchClick").on("click", function() {
+//         let pokemonCard = $("#textInput").val();
+//         console.log(pokemonCard)
+//         $("#textInput").val("")
 
-function apiPokemon(pokemonCard) {
-    // console.log('inside of apiPokemon function')
-    console.log(pokemonCard)
-    let requestURL = "https://api.pokemontcg.io/v1/cards?name=" + (pokemonCard || '').trim().toLowerCase();
-    $.ajax({
-        url: requestURL,
-        method: "GET",
-    }).then(function (data) {
-        console.log("data", data);
-        // console.log(data.cards[0].name);
-        // console.log(data.cards[0].imageUrl)
+//         if (!pokemonCard) {
+//       return;
+//     }
+
+//         apiPokemon(pokemonCard, true);
+//     });
+
+//     function apiPokemon(pokemonCard) {
+//     // console.log('inside of apiPokemon function')
+//     console.log(pokemonCard)
+//     let requestURL = "https://api.pokemontcg.io/v1/cards?name=" + (pokemonCard || '').trim().toLowerCase();
+//     $.ajax({
+//         url: requestURL,
+//         method: "GET",
+//     }).then(function (data) {
+//         console.log("data", data);
+//         // console.log(data.cards[0].name);
+//         // console.log(data.cards[0].imageUrl)
 
 
-        let pokemon = $("#pokemon-display");
-        let pokemonCard = $("<img>");
+//         let pokemon = $("#pokemon-display");
+//         let pokemonCard = $("<img>");
 
-        for (var i = 0; i < data.cards.length; i++) {
+//         for (var i = 0; i < data.cards.length; i++) {
 
-        pokemonCard.attr("src", "" + data.cards[i].imageUrl);
-        console.log(data.cards[i].imageUrl)
-        }
+//         pokemonCard.attr("src", "" + data.cards[i].imageUrl);
+//         console.log(data.cards[i].imageUrl)
+//         pokemon.append(pokemonCard);
+//         }
 
-        pokemon.append(pokemonCard);
-        $("#pokemon-display").empty();
-        // $("#pokemon-display").empty();
-        // pokemonCard.attr("src", "" + data.cards[0].imageUrl);
+//         $("#pokemon-display").empty();
+//         // $("#pokemon-display").empty();
+//         // pokemonCard.attr("src", "" + data.cards[0].imageUrl);
 
-        // pokemon.append(pokemonCard);
+//         // pokemon.append(pokemonCard);
 
-    });
+//     });
     
-}
-})
+// }
+// })
